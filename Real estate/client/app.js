ffb13ef18f794eb0f1fe3f3a0676b3b6f2cf8911
Abhinav NB <120695175+beenow011@users.noreply.github.com>
2023-06-02 23:33:06 +0530

    Add files via upload

---
+++ Real estate/client/app.js
@@ -0,0 +1,153 @@
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+
+}
+
+animateCircles();
+
+
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("door1")) {
+        image.src = "door2.jpg";
+        // image.classList.toggle('zoomed-in')
+        toggleDiv2()
+        
+        
+    }
+
+    else {
+        image.src = "door1.jpg";
+       toggleDiv1();
+
+    }
+}
+function toggleDiv1() {
+  var div = document.getElementById('myDiv');
+  div.style.display = (div.style.display === 'none') ? 'flex' : 'none';
+}
+function toggleDiv2() {
+  var div = document.getElementById('myDiv');
+  div.style.display = (div.style.display === 'flex') ? 'none' : 'flex';
+}
+
+function getBathValue() {
+  var uiBathrooms = document.getElementsByName("uiBathrooms");
+  for(var i in uiBathrooms) {
+    if(uiBathrooms[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function getBHKValue() {
+  var uiBHK = document.getElementsByName("uiBHK");
+  for(var i in uiBHK) {
+    if(uiBHK[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function onClickedEstimatePrice() {
+  console.log("Estimate price button clicked");
+  var sqft = document.getElementById("uiSqft");
+  var bhk = getBHKValue();
+  var bathrooms = getBathValue();
+  var location = document.getElementById("uiLocations");
+  var estPrice = document.getElementById("uiEstimatedPrice");
+
+  var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+  // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+
+  $.post(url, {
+      total_sqft: parseFloat(sqft.value),
+      bhk: bhk,
+      bath: bathrooms,
+      location: location.value
+  },function(data, status) {
+      console.log(data.estimated_price);
+      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+      console.log(status);
+  });
+}
+
+function onPageLoad() {
+  console.log( "document loaded" );
+  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+  // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  $.get(url,function(data, status) {
+      console.log("got response for get_location_names request");
+      if(data) {
+          var locations = data.locations;
+          var uiLocations =document.getElementById("uiLocations");
+          $('#uiLocations').empty();
+          for(var i in locations) {
+              var opt = new Option(locations[i]);
+              $('#uiLocations').append(opt);
+          }
+      }
+  });
+}
+
+window.onload = onPageLoad;
